b406cef414dc32cfb2b22a11dc021b8c4e50a31f
Brianna Veenstra <b.g.veenstra@gmail.com>
2017-02-21 11:15:16 -0800

    rm commented out code from search component

--- src/Search.js
+++ src/Search.js
@@ -2,23 +2,6 @@ import React, { Component } from 'react';
 import './Search.css';
 
 class Search extends Component {
-  // constructor (props) {
-  //   super()
-  //   this.state = {
-  //     query: ''
-  //   }
-  // }
-  //
-  // handleChange(event) {
-  //   this.setState({query: event.target.value});
-  // }
-  //
-  // handleSubmit(event) {
-  //   // setState is inherited from the Component class
-  //   event.preventDefault();
-  //   console.log(`searched! ${this.state.query}`);
-  // }
-
   render() {
     return (
       <div className="Search">
@@ -30,19 +13,7 @@ class Search extends Component {
           <input type="submit" value="Search"/>
         </form>
       </div>
-    )
-    // return (
-    //   <div className="Search">
-    //     <form className="Search-form" onSubmit={(e) => this.handleSubmit(e)}>
-    //       <input type="text"
-    //                 value={this.state.query}
-    //                 onChange={(e) => this.handleChange(e)}
-    //                 placeholder="enter search term" />
-    //       <br />
-    //       <input type="submit" />
-    //     </form>
-    //   </div>
-    // );
+    );
   }
 }
 
