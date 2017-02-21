c1ba612bf39ba81e86d3baf8019276b694d32605
Brianna Veenstra <b.g.veenstra@gmail.com>
2017-02-21 13:10:41 -0800

    initial with jquery errors

--- src/SearchContainer.js
+++ src/SearchContainer.js
@@ -3,6 +3,17 @@ import './SearchContainer.css';
 import Search from './Search';
 import Results from './Results';
 
+const searchGifs = (query) => {
+  // http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+  const searchUrl = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=";
+  let term = query.replace(/\s/, "+"); // replace any spaces with a "+"
+
+  // fetch all gifs matching the passed in phrase
+  return $.getJSON(searchUrl+term).then((response) =>
+    response["Search"]
+  )
+};
+
 class SearchContainer extends Component {
   constructor (props) {
     super()
@@ -20,47 +31,21 @@ class SearchContainer extends Component {
   handleSubmit(event) {
     event.preventDefault();
     console.log(`searched! ${this.state.query}`);
-    let searchResponse = { "data": [
-        {
-          "type": "gif",
-          "id": "iuHaJ0D7macZq",
-          "url": "http://giphy.com/gifs/cat-day-tomorrow-iuHaJ0D7macZq",
-          "source": "https://www.reddit.com/r/CatGifs/comments/5f0h9a/tomorrow_is_legs_day/",
-          "rating": "pg",
-          "images": {
-            "fixed_height": {
-              "url": "http://media4.giphy.com/media/iuHaJ0D7macZq/200.gif"
-            }
-          }
-        },
-        {
-          "type": "gif",
-          "id": "Z1kpfgtHmpWHS",
-          "url": "http://giphy.com/gifs/cat-way-make-Z1kpfgtHmpWHS",
-          "source": "http://shewhoseeks.blogspot.com/2016/03/cat-gifs-that-make-me-laugh-way-more.html",
-          "rating": "g",
-          "images": {
-            "fixed_height": {
-              "url": "http://media4.giphy.com/media/Z1kpfgtHmpWHS/200.gif"
-            }
-          }
-        }
-      ],
-      "meta": {
-        "status": 200,
-        "msg": "OK"
-      },
-      "pagination": {
-        "total_count": 1947,
-        "count": 25,
-        "offset": 0
-      }
-    };
+    searchGifs(this.state.query).then((response) => {
+      console.log(response);
+    //   // redefine our app's state to include populated response
+    //   this.setState({
+    //     results: movies,
+    //     query: "",
+    //     searched: true // flip the switch
+    //   })
+    });
     this.setState({
       searched: true,
       results: searchResponse.data
     });
   }
+
   render() {
     return (
       <div className="SearchContainer">
